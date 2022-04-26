b91d6af3b912f970cf954b7bbda61e621a598d71
Iván Portillo Perez <ivan.portillo@unir.net>
2022-04-26 17:50:18 +0200

    Creación de entidad escuela

---
+++ Entidades/Escuela.cs
@@ -0,0 +1,25 @@
+namespace CoreEscuela.Entidades
+{
+    class Escuela
+    {
+        public string Nombre{ get; set; }
+        public int AnoDeCreacion{ get; set; }
+        public string Pais { get; set; }
+        public string Ciudad { get; set; }
+        
+        public TiposEscuela TipoEscuela { get; set; }
+        public Escuela(string nombre, int año) => (Nombre, AnoDeCreacion) = (nombre, año);
+
+        public Escuela(string nombre, int año, TiposEscuela tipo, string pais = "", string ciudad = "")
+        {
+            (Nombre, AnoDeCreacion) = (nombre, año);
+            Pais = pais;
+            Ciudad = ciudad;
+        }
+
+        public override string ToString()
+        {
+            return $"Nombre: {Nombre}, Tipo: {TipoEscuela} \n Pais: {Pais}, Ciudad: {Ciudad} \n Fundada: {AnoDeCreacion}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CoreEscuela.Entidades;
+
+namespace Etapa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var escuela = new Escuela("Binary School", 1981, TiposEscuela.PreEscolar, pais:"España", ciudad: "Fuenlabrada");
+            escuela.Ciudad = "Fuenlabrada";
+
+        //    "Bienvenido a la escuela "  + escuela.Nombre + " .Creada el año " + escuela.AnoDeCreacion
+            Console.WriteLine(escuela);
+        }
+    }
+}
